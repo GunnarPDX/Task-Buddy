6a17fd101aab2d7e3b92c96bd36d76780f9828eb
Gunnar R <gunnarrosenberg@gmail.com>
2020-11-11 23:45:06 -0800

    add migration for tasks position

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_070642) do
+ActiveRecord::Schema.define(version: 2020_11_12_074438) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_11_12_070642) do
     t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
